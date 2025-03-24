e2b68ea9116e7bc8b0a2b225d6c9ad7fea1a0ce5
Suyash29-source <suyash2v9@gmail.com>
2025-03-24 17:46:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 import telebot
 import os
 
+# ✅ Bot Token
 API_TOKEN = "7806071446:AAFukCv3jKDCM8cQKnk0UevHzGjCl5QD13E"
 bot = telebot.TeleBot(API_TOKEN)
 
+# ✅ Flask App
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,7 +18,7 @@ def webhook():
     json_update = request.get_json()
     if json_update:
         bot.process_new_updates([telebot.types.Update.de_json(json_update)])
-    return "!", 200
+    return "OK", 200
 
 if __name__ == "__main__":
     # ✅ Pehle Purana Webhook Delete Karo
@@ -25,5 +27,5 @@ if __name__ == "__main__":
     # ✅ Naya Webhook Set Karo
     bot.set_webhook(url=f"https://chatbook-58zq.onrender.com/{API_TOKEN}")
 
-    # ✅ Flask Start Karo (Background me bot chalega)
+    # ✅ Flask Start Karo
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
